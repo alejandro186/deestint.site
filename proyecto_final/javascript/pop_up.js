000db06d31e9ecd8c3dbc41f2512e298c26fc14b
alejandro186 <1alejodiaz09@gmail.com>
2026-02-08 15:58:51 -0500

    Implement WhatsApp support button in pop-up
    Added WhatsApp support button functionality to pop-up.

--- proyecto_final/javascript/pop_up.js
+++ proyecto_final/javascript/pop_up.js
@@ -23,7 +23,7 @@ botones.forEach(boton => {
           <p> ${descripcion_2} </p>
         </div>
         <section class="especificaciones_importantes">
-          <button class="more_information" onclick="">
+          <button  id="boton_whatsapp_soporte_producto" class="more_information">
             <i class="fa-solid fa-headset"></i>
           </button>
           <h3 class="m_precio"> ${precio} </h3>
@@ -34,6 +34,20 @@ botones.forEach(boton => {
       `
     pop_up.classList.add("visible");
     overlay_4.style.background = bakcground_color;
+
+    const boton_whatsapp = document.getElementById('boton_whatsapp_soporte_producto');
+    boton_whatsapp.addEventListener('click', function(){
+        if(!nombre){
+            alert("primero seleccione un producto");
+            return;
+        } else{
+        const telefono = "573185168373";
+        let mensaje = `hola, requiero soporte para este producto: ${nombre}`;
+        const url = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
+
+        window.open(url, '_blank');
+        };
+    });
   });
 });
 
